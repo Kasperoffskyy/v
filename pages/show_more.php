845ad2c2329098a99fe51ba634d83f0bd3d2c7f8
Kasperoffskyy <31186939+Kasperoffskyy@users.noreply.github.com>
2017-08-30 16:55:09 +0600

    Update show_more.php

--- pages/show_more.php
+++ pages/show_more.php
@@ -20,6 +20,13 @@ if (!$orders = redisAction(false, 'show-more'.$pageParam.$pgSm, 'get')){
   redisAction($orders, 'show-more'.$pageParam.$pgSm, 'set');
 }
 
+//Избавляемся от ненужных html символов
+foreach ($orders as &$order){
+  foreach ($order as &$a){
+   $a = htmlspecialchars($a);
+  }
+}
+
 echo json_encode([
   "content" => $orders
 ]);
